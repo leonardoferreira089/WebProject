9d8a7052388438bdfeb6b8045e473f85666ca614
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2020-08-17 10:06:09 -0400

    Seeding Service

--- SalesWebMvc/Models/Enums/SaleStatus.cs
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -5,7 +5,7 @@ namespace SalesWebMvc.Models.Enums
     public enum SaleStatus : int
     {
         Pending = 0,
-        billed = 1,
-        Canceld = 2
+        Billed = 1,
+        Canceled = 2
     }
 }
