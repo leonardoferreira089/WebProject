17173d47d2e5166f961040a5331995d90fb57b77
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2020-08-21 11:58:24 -0400

    Returning custom error page

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Models;
-using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
 namespace SalesWebMvc.Services
